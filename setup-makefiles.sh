2050846eb9801edf3b5eca811f26f8def47eec1c
sickkside13 <sickksidex3@gmail.com>
2017-02-14 14:50:01 -0600

    Update setup-makefiles.sh
     Use msm8996-common extraction scripts

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -19,6 +19,7 @@ set -e
 
 export DEVICE=h850
 export DEVICE_COMMON=g5-common
+export PLATFORM_COMMON=msm8996-common
 export VENDOR=lge
 
-./../$DEVICE_COMMON/setup-makefiles.sh $@
+./../$PLATFORM_COMMON/setup-makefiles.sh $@
